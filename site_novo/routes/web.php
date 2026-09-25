d3a79c3e46a528d75d134115513eb2b3157eb2e5
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2026-09-25 15:08:15 -0300

    feat: adiciona rota e página pública /palestrantes

--- site_novo/routes/web.php
+++ site_novo/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\JobListingController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SpeakerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,5 @@ Route::get('/blog/{slug}', [PostController::class, 'show'])->name('blog.show');
 Route::get('/eventos', [EventController::class, 'index'])->name('events.index');
 
 Route::get('/vagas', [JobListingController::class, 'index'])->name('jobs.index');
+
+Route::get('/palestrantes', [SpeakerController::class, 'index'])->name('speakers.index');
